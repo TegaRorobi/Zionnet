8d1375b3657104cab21af175f55daf7ac77cce08
Tega Rorobi <rorobitega@gmail.com>
2023-11-28 13:25:45 +0100

    refactor: Modified the helpers.fields.ExtensionAwareImageField.__init__ method, ran migrations

--- helpers/fields.py
+++ helpers/fields.py
@@ -7,5 +7,5 @@ class ExtensionAwareImageField(models.ImageField):
     def __init__(self, *args, **kwargs):
         allowed_extensions = kwargs.get('extensions', ('png', 'jpg', 'jpeg', 'jfif'))
         extension_validator = FileExtensionValidator(allowed_extensions=(allowed_extensions))
-        kwargs['validators'] = list(kwargs.get('validators', [])) + [extension_validator]
+        kwargs.setdefault('validators', [extension_validator])
         return super().__init__(*args, **kwargs)
