1cc2d038e94d5fb590f0ec5c55122a9ab06ac6cd
Mahlet Seifu <akotet2123@gmail.com>
2023-11-28 17:43:40 +0300

    feature to retrieve all and popular categories

--- BusinessDirectory/urls.py
+++ BusinessDirectory/urls.py
@@ -1,6 +1,22 @@
 from django.urls import path
-from .views import BusinessListingListCreateView
+from .views import (
+    BusinessListingCategoryListView,
+    BusinessListingListCreateView,
+    PopularBusinessListingCategoryListView,
+)
 
 urlpatterns = [
-    path('api/listings/', BusinessListingListCreateView.as_view(), name='business-listings'),
+    path(
+        "listings/", BusinessListingListCreateView.as_view(), name="business-listings"
+    ),
+    path(
+        "categories/",
+        BusinessListingCategoryListView.as_view(),
+        name="business-categories",
+    ),
+    path(
+        "categories/popular/",
+        PopularBusinessListingCategoryListView.as_view(),
+        name="popular-category-list",
+    ),
 ]
--- BusinessDirectory/views.py
+++ BusinessDirectory/views.py
@@ -2,10 +2,12 @@ from rest_framework import generics, filters, status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from .models import BusinessListing
+from .models import BusinessListing, BusinessListingCategory
 from .pagination import ListingPagination
-from .serializers import BusinessListingSerializer
+from .serializers import BusinessListingCategorySerializer, BusinessListingSerializer
 from .permissions import IsVendorVerified
+from django.db.models import Count
+
 
 class BusinessListingListCreateView(generics.ListCreateAPIView):
     """
@@ -15,32 +17,37 @@ class BusinessListingListCreateView(generics.ListCreateAPIView):
     Requires authentication for GET requests. For POST requests,a vendor profile
     verification is required.
     """
+
     queryset = BusinessListing.objects.all()
     serializer_class = BusinessListingSerializer
     filter_backends = [filters.SearchFilter, filters.OrderingFilter]
-    search_fields = ['name', 'description', 'city', 'province', 'country',
-    'phone_number']
-    ordering_fields = ['name', 'created_at']  
-    ordering = ['-created_at']  
+    search_fields = [
+        "name",
+        "description",
+        "city",
+        "province",
+        "country",
+        "phone_number",
+    ]
+    ordering_fields = ["name", "created_at"]
+    ordering = ["-created_at"]
     pagination_class = ListingPagination
     permission_classes = [IsVendorVerified]
-    
-    
+
     def retrieve(self, request, *args, **kwargs):
-        # retrieves an instance of a listing and attach the related images for the listing 
+        # retrieves an instance of a listing and attach the related images for the listing
         instance = self.get_object()
         serializer = self.get_serializer(instance)
-        
+
         images = BusinessListingImage.objects.filter(listing=instance)
         image_serializer = BusinessListingImageSerializer(images, many=True)
         response_data = serializer.data
-        response_data['images'] = image_serializer.data
+        response_data["images"] = image_serializer.data
 
         return Response(response_data)
-        
-        
+
     def list(self, request, *args, **kwargs):
-        # getting list of listings for an authenticated get request with paginated response 
+        # getting list of listings for an authenticated get request with paginated response
         queryset = self.filter_queryset(self.get_queryset())
         page = self.paginate_queryset(queryset)
         if page is not None:
@@ -58,25 +65,29 @@ class BusinessListingListCreateView(generics.ListCreateAPIView):
             rest_framework.exceptions.ValidationError: If validation fails for the main listing details.
         Creates a new BusinessListing and associates images and files with it.
         """
-        images_data = self.request.FILES.getlist('images', [])
-        files_data = self.request.FILES.getlist('files', [])
+        images_data = self.request.FILES.getlist("images", [])
+        files_data = self.request.FILES.getlist("files", [])
 
         # Save associated images
         for image_data in images_data:
-            image_serializer = BusinessListingImageSerializer(data={'image': image_data, 'listing': business_listing.id})
+            image_serializer = BusinessListingImageSerializer(
+                data={"image": image_data, "listing": business_listing.id}
+            )
             image_serializer.is_valid(raise_exception=True)
             image_serializer.save()
-        
-        #save associated files 
+
+        # save associated files
         for file_data in files_data:
-            file_serializer = BusinessListingFileSerializer(data={'file': file_data, 'listing': business_listing.id})
+            file_serializer = BusinessListingFileSerializer(
+                data={"file": file_data, "listing": business_listing.id}
+            )
             file_serializer.is_valid(raise_exception=True)
             file_serializer.save()
-        
+
         # save an instance of business listing
         serializer.is_valid(raise_exception=True)
         business_listing = serializer.save(vendor_id=self.request.user)
-       
+
     def post(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
@@ -85,4 +96,30 @@ class BusinessListingListCreateView(generics.ListCreateAPIView):
         except Response as response:
             return response
         headers = self.get_success_headers(serializer.data)
-        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+        return Response(
+            serializer.data, status=status.HTTP_201_CREATED, headers=headers
+        )
+
+
+class BusinessListingCategoryListView(APIView):
+    def get(self, request, format=None):
+        """
+        Retrieve all business listing categories from the database
+        """
+        categories = BusinessListingCategory.objects.all()
+        serializer = BusinessListingCategorySerializer(categories, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class PopularBusinessListingCategoryListView(APIView):
+    def get(self, request, format=None):
+        """
+        Retrieve popular business listing categories
+        popularity is based on the number of associated businesses
+        """
+        popular_categories = BusinessListingCategory.objects.annotate(
+            num_businesses=Count("listings")
+        ).order_by("-num_businesses")
+
+        serializer = BusinessListingCategorySerializer(popular_categories, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
