71809e8643eff61bd58be31e1bbcc5da0ee6a466
Tega Rorobi <rorobitega@gmail.com>
2023-11-28 12:44:14 +0100

    feat: Added a 'Store' model, registered it in the admin, ran migrations

--- MarketPlace/admin.py
+++ MarketPlace/admin.py
@@ -4,3 +4,8 @@ from .models import *
 class MarketplaceAdmin(admin.ModelAdmin):
     model = MarketPlace
     list_display = 'name', 'cover_image'
+
+
+class StoreAdmin(admin.ModelAdmin):
+    model = Store 
+    list_display = 'name', 'marketplace', 'vendor', 'country', 'city'
--- MarketPlace/models.py
+++ MarketPlace/models.py
@@ -12,3 +12,19 @@ class MarketPlace(TimestampsModel):
 
     def __str__(self) -> str:
         return self.name
+
+
+class Store(TimestampsModel):
+    marketplace = models.ForeignKey(MarketPlace, related_name='stores', on_delete=models.PROTECT)
+    vendor = models.ForeignKey(User, verbose_name=_('store vendor'), related_name='stores', on_delete=models.CASCADE)
+    name = models.CharField(_('store name'), max_length=255)
+    description = models.TextField(_('store description'), null=True, blank=True)
+    logo = ExtensionAwareImageField(upload_to='store/logos', null=True, blank=True)
+    cover_image = ExtensionAwareImageField(upload_to='store/cover_images', null=True, blank=True)
+    country = models.CharField(_('country of location'), max_length=255)
+    city = models.CharField(_('store city'), max_length=255)
+    province = models.CharField(_('store province'), max_length=255)
+    additional_information = models.JSONField(_('store additional information'), null=True, blank=True)
+
+    def __str__(self) -> str:
+        return self.name.__str__()
