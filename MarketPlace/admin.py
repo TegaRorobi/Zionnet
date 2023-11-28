0e8391a39870c6f5e4870e8445c2dc16aaf842db
Tega Rorobi <rorobitega@gmail.com>
2023-11-28 13:06:12 +0100

    feat: Added 'ProductImage', 'ProductRating' and 'ProductReaction' models, registered them in the admin, ran migrations

--- MarketPlace/admin.py
+++ MarketPlace/admin.py
@@ -1,20 +1,37 @@
 from django.contrib import admin
 from .models import *
 
+@admin.register(MarketPlace)
 class MarketplaceAdmin(admin.ModelAdmin):
     model = MarketPlace
     list_display = 'name', 'cover_image'
 
-
+@admin.register(Store)
 class StoreAdmin(admin.ModelAdmin):
     model = Store 
     list_display = 'name', 'marketplace', 'vendor', 'country', 'city'
 
-
+@admin.register(ProductCategory)
 class ProductCategoryAdmin(admin.ModelAdmin):
     model = ProductCategory
     list_display = 'name', 'image'
 
+@admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     model = Product
     list_display = 'name', 'merchant', 'category', 'store', 'price'
+
+@admin.register(ProductImage)
+class ProductImageAdmin(admin.ModelAdmin):
+    model = ProductImage
+    list_display = 'product', 'image'
+
+@admin.register(ProductRating)
+class ProductRatingAdmin(admin.ModelAdmin):
+    model = ProductRating
+    list_display = 'user', 'product', 'value'
+
+@admin.register(ProductReaction)
+class ProductReactionAdmin(admin.ModelAdmin):
+    model = ProductReaction
+    list_display = 'user', 'product', 'reaction'
--- MarketPlace/models.py
+++ MarketPlace/models.py
@@ -60,3 +60,33 @@ class Product(TimestampsModel):
 
     def __str__(self) -> str:
         return self.name
+
+
+class ProductImage(TimestampsModel):
+    product = models.ForeignKey(Product, related_name='images', on_delete=models.CASCADE)
+    image = ExtensionAwareImageField(_('image file'), upload_to='products/product_images')
+
+    def __str__(self) -> str:
+        return 'Image representation of ' + self.product.__str__()
+
+
+class ProductRating(TimestampsModel):
+    user = models.ForeignKey(User, related_name='product_ratings', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='ratings')
+    value = models.PositiveSmallIntegerField(default=5, validators=[MinValueValidator(1), MaxValueValidator(5)])
+
+    def __str__(self) -> str:
+        return f"{self.user.__str__()} rates {self.product.__str__()} a {self.value}/5"
+
+
+class ProductReaction(TimestampsModel):
+    REACTION_CHOICES = [
+        ('like', 'like'),
+        ('dislike', 'dislike')
+    ]
+    user = models.ForeignKey(User, related_name='product_reactions', on_delete=models.SET_NULL, null=True, blank=True)
+    product = models.ForeignKey(Product, related_name='reactions', on_delete=models.CASCADE)
+    reaction = models.CharField(max_length=10, choices=REACTION_CHOICES)
+
+    def __str__(self) -> str:
+        return f"{self.product.__str__()} {self.reaction}d by {self.reactor.__str__() or 'AnonymousUser'}"
