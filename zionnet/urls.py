586ea61e2d85045a0847d51e1ac7cc1d1aa64534
Tega Rorobi <rorobitega@gmail.com>
2023-11-28 20:20:21 +0100

    feat: Added static urls to the root urlconf

--- zionnet/urls.py
+++ zionnet/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from django.urls import include, path
+from django.conf.urls.static import static
+from django.conf import settings
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
@@ -46,5 +47,7 @@ urlpatterns = [
     path('accounts/', include('authentication.urls')),
     path('api/', include('BusinessDirectory.urls')),
     path('api/', include('MarketPlace.urls')),
-    
+
+    *static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
+    *static(settings.STATIC_URL, document_root=settings.STATIC_ROOT),
 ]
