2a5bb22da470a7b5311c968167ed3fe0b32ca22a
Tega Rorobi <rorobitega@gmail.com>
2023-11-28 23:55:22 +0100

    feat: Created a serializer, view and urlconf for getting product categories within a marketplace

--- MarketPlace/urls.py
+++ MarketPlace/urls.py
@@ -3,5 +3,14 @@ from django.urls import re_path
 from .views import *
 
 urlpatterns = [
-    re_path('^marketplace/all/?$', GetAllMarketPlacesView.as_view(), name='marketplaces-list')
+    re_path(
+        '^marketplace/all/?$',
+        GetAllMarketPlacesView.as_view(),
+        name='marketplaces-list'
+    ),
+    re_path(
+        '^marketplace/(?P<pk>\d+)/products/categories/?$',
+        GetAllProductCategoriesView.as_view(),
+        name='marketplace-product-categories-list'
+    )
 ]
--- MarketPlace/views.py
+++ MarketPlace/views.py
@@ -1,5 +1,5 @@
 
-from rest_framework import generics, status, permissions
+from rest_framework import generics, status, permissions, pagination
 from rest_framework.response import Response
 from .serializers import *
 from .models import *
@@ -19,3 +19,17 @@ class GetAllMarketPlacesView(generics.GenericAPIView):
         serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
+
+class GetAllProductCategoriesView(generics.GenericAPIView):
+
+    "API View to get all product categories within a marketplace"
+
+    queryset = MarketPlace.objects.order_by('-id')
+    permission_classes = [permissions.AllowAny]
+    serializer_class = ProductCategorySerializer
+
+    def get(self, request, *args, **kwargs):
+        marketplace = self.get_object()
+        product_categories = marketplace.product_categories.all()
+        serializer = self.get_serializer(product_categories, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
