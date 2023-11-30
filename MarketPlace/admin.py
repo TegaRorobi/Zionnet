4c50c840de4af2b2f2be92c272546accfe92a4c8
Tega Rorobi <rorobitega@gmail.com>
2023-11-30 18:04:32 +0100

    chore: Updated the MarketPlace admin

--- MarketPlace/admin.py
+++ MarketPlace/admin.py
@@ -3,22 +3,18 @@ from .models import *
 
 @admin.register(MarketPlace)
 class MarketplaceAdmin(admin.ModelAdmin):
-    model = MarketPlace
     list_display = 'name', 'cover_image'
 
 @admin.register(Store)
 class StoreAdmin(admin.ModelAdmin):
-    model = Store 
     list_display = 'name', 'marketplace', 'vendor', 'country', 'city'
 
 @admin.register(ProductCategory)
 class ProductCategoryAdmin(admin.ModelAdmin):
-    model = ProductCategory
     list_display = 'name', 'image'
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    model = Product
     list_display = 'name', '_vendor', 'category', 'store', 'price'
 
     @admin.display()
@@ -27,27 +23,22 @@ class ProductAdmin(admin.ModelAdmin):
 
 @admin.register(ProductImage)
 class ProductImageAdmin(admin.ModelAdmin):
-    model = ProductImage
     list_display = 'product', 'image'
 
 @admin.register(ProductRating)
 class ProductRatingAdmin(admin.ModelAdmin):
-    model = ProductRating
     list_display = 'user', 'product', 'value'
 
 @admin.register(ProductReaction)
 class ProductReactionAdmin(admin.ModelAdmin):
-    model = ProductReaction
     list_display = 'user', 'product', 'reaction'
 
 @admin.register(Cart)
 class CartAdmin(admin.ModelAdmin):
-    model = Cart
     list_display = 'owner', 'created_at'
 
 @admin.register(CartItem)
 class CartItemAdmin(admin.ModelAdmin):
-    model = CartItem
     list_display = 'cart_owner_', 'product', 'quantity'
 
     @admin.display()
@@ -56,5 +47,8 @@ class CartItemAdmin(admin.ModelAdmin):
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    model = Order
     list_display = 'buyer', 'product', 'quantity', 'status'
+
+@admin.register(StoreVendor)
+class StoreVendorAdmin(admin.ModelAdmin):
+    list_display = 'user', 'email', 'is_approved'
