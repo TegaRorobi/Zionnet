91fefd353dd3b1397c3a23463155f38a04ebc140
Tega Rorobi <rorobitega@gmail.com>
2023-12-08 13:47:03 +0100

    fix: Removed merge conflict markers, modified serializers

--- JobPosting/serializers.py
+++ JobPosting/serializers.py
@@ -1,25 +1,13 @@
-<<<<<<< HEAD
 
 from rest_framework import serializers
 from .models import *
 
-
-class FreelancerProfileSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = FreelancerProfile
-        fields = '__all__'
-        extra_kwargs = {
-            'user': {'read_only':True},
-        }
-
-=======
-from rest_framework import serializers
-from .models import *
 
 class CompanySocialLinkSerializer(serializers.ModelSerializer):
     class Meta:
         model = CompanySocialLink
         fields = ['social_name', 'social_link']
+
 
 class CompanySerializer(serializers.ModelSerializer):
     social_links = CompanySocialLinkSerializer(many=True, read_only=True)
@@ -35,29 +23,36 @@ class JobSkillSerializer(serializers.ModelSerializer):
         model = JobSkill
         fields = ['name']
 
+
 class JobRoleSerializer(serializers.ModelSerializer):
     class Meta:
         model = JobRole
         fields = ['name']
+
 
 class JobCategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = JobCategory
         fields = ['name']
 
+
 class FreelancerSocialLinkSerializer(serializers.ModelSerializer):
     class Meta:
         model = FreelancerSocialLink
         fields = ['social_name', 'social_link']
 
+
 class FreelancerProfileSerializer(serializers.ModelSerializer):
-    skills = JobSkillSerializer(many=True, read_only=True)
-    social_links = FreelancerSocialLinkSerializer(many=True, read_only=True)
+    skills = JobSkillSerializer(many=True, required=False)
+    social_links = FreelancerSocialLinkSerializer(many=True, required=False)
 
     class Meta:
         model = FreelancerProfile
-        fields = ['id', 'user', 'skills', 'title', 'bio', 'profile_pic', 'banner_image', 'resume',
-                  'preferred_time_commitment', 'preferred_presence_type', 'experience_range', 'social_links']
+        fields = '__all__'
+        extra_kwargs = {
+            'user': {'read_only':True},
+        }
+
 
 class JobOpeningSerializer(serializers.ModelSerializer):
     required_skills = JobSkillSerializer(many=True, read_only=True)
@@ -67,28 +62,24 @@ class JobOpeningSerializer(serializers.ModelSerializer):
         fields = ['id', 'company', 'role', 'category', 'poster', 'poster_positon', 'title', 'description',
                   'time_commitment', 'presence_type', 'experience_range', 'required_skills', 'resumption_date',
                   'contract_period', 'hourly_rate', 'hourly_rate_currency']
->>>>>>> ee2d3c040ba3840553df8a6a3ed01dcfabaad546
+
 
 class JobApplicationSerializer(serializers.ModelSerializer):
     class Meta:
         model = JobApplication
-<<<<<<< HEAD
         fields = '__all__'
         extra_kwargs = {
             'applicant': {'read_only':True}
         }
 
-=======
-        fields = ['id', 'job_opening', 'applicant', 'first_name', 'last_name', 'email', 'phone_number', 'application_info', 'resume']
 
 class JobRatingSerializer(serializers.ModelSerializer):
     class Meta:
         model = JobRating
         fields = ['value', 'user', 'job_opening']
 
+
 class JobReviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = JobReview
         fields = ['user', 'job', 'comment']
-        
->>>>>>> ee2d3c040ba3840553df8a6a3ed01dcfabaad546
